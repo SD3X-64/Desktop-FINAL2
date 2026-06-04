f206ec85d31480b02bfdf48f4a85a7e48d7ebeef
Студент ШС <@step.lan>
2026-06-04 17:22:07 +0700

    мне уже плохо

--- DatabaseInitializer.cs
+++ DatabaseInitializer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace Desktop_FINAL2
 {
