bcae6cb4e943f9ed9015054eb0864a242887c08a
Студент ШС <@step.lan>
2026-06-04 16:43:08 +0700

    Создал проект

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Desktop_FINAL2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
