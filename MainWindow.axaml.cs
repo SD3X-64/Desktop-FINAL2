35994df325a847820b68c6c9e57d4d6a50d32e96
Студент ШС <@step.lan>
2026-06-11 17:50:22 +0700

    На Сегодня Всё!

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,18 +1,32 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using Tmds.DBus.Protocol;
 
 namespace Desktop_FINAL2
 {
     public partial class MainWindow : Window
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         static public string ConnectionString =
             @"Server=(localdb)\mssqllocaldb;
               Database=UniversityDB;
               Integrated Security=True;
               TrustServerCertificate=True;";
+
+        public string buttonText = "Initialize Database";
+        public string ButtonText
+        {
+            get { return buttonText; }
+            set
+            {
+                buttonText = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonText)));
+            }
+        }
 
         public MainWindow()
         {
@@ -22,9 +36,15 @@ namespace Desktop_FINAL2
 
         public void DBInit(object? sender, RoutedEventArgs e)
         {
+            ButtonText = "Initializing Database";
             DatabaseInitializer dbinit = new DatabaseInitializer();
+            ButtonText = "Initializing Database.";
             dbinit.InitTables(ConnectionString);
+            ButtonText = "Initializing Database..";
             dbinit.TestData(ConnectionString);
+            ButtonText = "Initializing Database...";
+            ButtonText = "Done!";
+            ButtonText = buttonText;
         }
     }
 }
