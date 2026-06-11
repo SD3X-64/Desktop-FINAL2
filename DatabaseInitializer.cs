042d31ab82c2c2207c05ff3bfe897af3b5b1d58a
Студент ШС <@step.lan>
2026-06-11 16:42:17 +0700

    Прогресс есть

--- DatabaseInitializer.cs
+++ DatabaseInitializer.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Desktop_FINAL2
 {
@@ -20,7 +21,7 @@ namespace Desktop_FINAL2
 
             [Required]
             [MaxLength(200)]
-            public string Name { get; set; } = string.Empty;
+            public string Title { get; set; } = string.Empty;
 
             [Required]
             [MaxLength(200)]
@@ -34,7 +35,7 @@ namespace Desktop_FINAL2
             public int? Episodes { get; set; }
             public string? AnimeStatus { get; set; }
             public string? WatchStatus { get; set; }
-            public int? LastWatchedEpisode { get; set; }
+            public int? LastWatched { get; set; }
         }
 
         public class Studio
@@ -55,6 +56,50 @@ namespace Desktop_FINAL2
             [Required]
             [MaxLength(200)]
             public string Name { get; set; } = string.Empty;
+        }
+
+        public void InitTables(string ConnectionString)
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            string sqlQuery = @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Titles')
+            CREATE TABLE Titles (
+                Id INT PRIMARY KEY IDENTITY(1,1),
+                Title NVARCHAR(200) NOT NULL,
+                StudioName NVARCHAR(200) NOT NULL,
+                GenreName NVARCHAR(200) NOT NULL,
+                YearStarted DATE,
+                Seasons INT,
+                Episodes INT,
+                AnimeStatus NVARCHAR,
+                WatchStatus NVARCHAR,
+                LastWatched INT,
+                )";
+
+            string sqlQuery1 = @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Studios')
+            CREATE TABLE Studios (
+                Id INT PRIMARY KEY IDENTITY(1,1),
+                Name NVARCHAR(200) NOT NULL,
+                )";
+
+            string sqlQuery2 = @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Genres')
+            CREATE TABLE Genres (
+                Id INT PRIMARY KEY IDENTITY(1,1),
+                Name NVARCHAR(200) NOT NULL,
+                )";
+
+            using var command = new SqlCommand(sqlQuery, connection);
+            command.ExecuteNonQuery();
+
+            using var command1 = new SqlCommand(sqlQuery1, connection);
+            command1.ExecuteNonQuery();
+
+            using var command2 = new SqlCommand(sqlQuery2, connection);
+            command2.ExecuteNonQuery();
         }
     }
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,14 +1,28 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using System;
+using Tmds.DBus.Protocol;
 
 namespace Desktop_FINAL2
 {
     public partial class MainWindow : Window
     {
+
+        static public string ConnectionString =
+            @"Server=(localdb)\mssqllocaldb;
+              Database=UniversityDB;
+              Integrated Security=True;
+              TrustServerCertificate=True;";
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+        }
+
+        public void DBInit(object? sender, RoutedEventArgs e)
+        {
+            DatabaseInitializer.InitTables(ConnectionString);
         }
     }
 }
